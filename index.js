e17c1179cab136a6e1a4c58be51a12bd61d95c9e
dadbass <45670534+dadbass@users.noreply.github.com>
2018-12-09 16:13:32 -0800

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const Discord = require("discord.js");
 const client = new Discord.Client();
 client.on('ready', () => {
-  client.user.setGame(`*help | By JustSad #9998`,'https://www.twitch.tv/v5bz');
+  client.user.setGame(`*help | By 👑 éMyyy#9998`,'https://www.twitch.tv/v5bz');
   console.log('---------------');
   console.log(' Bot Is Online')
   console.log('---------------')
